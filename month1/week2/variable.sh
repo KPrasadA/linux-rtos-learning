e1186c56de9dfe2fccbd78afc0bf70c1d7c216ff
KPrasadA <krishnaprasad9798@gmail.com>
2026-07-04 08:23:26 +0000

    Bash variable initial tests

---
+++ month1/week2/variable.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+ 
+ #variable in bash
+ 
+ name="Krishnaprasad"
+
+ age=22
+ city="Bengaluru"
+
+ echo "Name : $name "
+echo "Age : $age"
+echo "City ; $city"
+
+
+text="HelloWorld"
+
+echo "Length : ${#text}"
+echo "Uppercase : ${text^^}"
